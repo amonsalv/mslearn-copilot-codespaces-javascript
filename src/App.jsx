87dcc14ea5e30f6f2ca90b2f836b996a58ae5079
Ana Monsalve <91292255+amonsalv@users.noreply.github.com>
2023-11-22 14:50:24 +0000

    Update siteProps with new information

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Ana María Monsalve",
+  title: "Full Stack Developer",
+  email: "amonsalve@cesde.edu.co",
+  gitHub: "amonsalv",
+  instagram: "_amonsalv",
+  linkedIn: "anamonsalve",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
